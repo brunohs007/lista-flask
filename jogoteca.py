8dcc36ac33d3b1c5f4e2fdaf60ca64d0bc012057
brunohs007 <brunoholandasilva@hotmail.com>
2022-12-21 15:32:17 -0300

    primeiro commit

---
+++ jogoteca.py
@@ -0,0 +1,44 @@
+from flask import Flask, render_template, request, redirect
+
+class Jogo:
+    def __init__(self, nome, categoria, console):
+        self.nome=nome
+        self.categoria=categoria
+        self.console=console
+
+jogo1 = Jogo('tetris', 'Puzzle', 'Atari')
+jogo2 = Jogo('Mario', 'Plataforma', 'Nintendo')
+jogo3 = Jogo('Crash', 'Aventura', 'Playstation')
+lista = [jogo1, jogo2, jogo3]
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('lista.html', titulo='Jogos', jogos=lista)
+
+@app.route('/novo')
+def novo():
+    return render_template('novo.html', titulo='Novo Jogo')
+
+@app.route('/criar', methods=['POST',])
+def criar():
+    nome = request.form['nome']
+    categoria = request.form['categoria']
+    console = request.form['console']
+    jogo = Jogo(nome, categoria, console)
+    lista.append(jogo)
+    return redirect('/')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/autenticar', methods=['POST',])
+def autenticar():
+    if 'admin' == request.form['senha']:
+        return redirect('/')
+    else:
+        return redirect('/login')
+
+app.run(debug=True)
